240b91b61d1e8fc297a34c638e583b49cf906db6
Devanshi <acouanon19@gmail.com>
2025-04-11 20:16:10 +0530

    Made pages folder Students js file

---
+++ sponsor-ed/src/Pages/Students.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import StudentForm from '../components/StudentForm'; // Import the form
+
+function Students() {
+  return (
+    <div>
+      {/* You could add other student-related content here later */}
+      <StudentForm />
+    </div>
+  );
+}
+
+export default Students;
